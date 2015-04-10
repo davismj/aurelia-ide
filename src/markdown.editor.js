5e72ec861da8eba2eec5c494078ae428535ea1ef
Matthew James Davis <davis.matthewjames@gmail.com>
2015-04-10 14:19:04 -0400

    Update markdown.editor.js

--- src/markdown.editor.js
+++ src/markdown.editor.js
@@ -4,7 +4,7 @@ import { inject } from 'aurelia-framework';
 export class MarkdownEditor{
 
 	constructor(element){
-    this.element = element;
-    this.mymarkdowntext = ![WAT](http://i0.kym-cdn.com/photos/images/newsfeed/000/173/576/Wat8.jpg "WAT")';
+        this.element = element;
+    	this.mymarkdowntext = '#WAT ![](http://i0.kym-cdn.com/photos/images/newsfeed/000/173/576/Wat8.jpg)';
 	}
 }
