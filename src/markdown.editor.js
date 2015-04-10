41ed2e8f58f2b69638137d8d4f72aaf84f3e83fa
Matthew James Davis <davis.matthewjames@gmail.com>
2015-04-10 14:20:02 -0400

    Update markdown.editor.js

--- src/markdown.editor.js
+++ src/markdown.editor.js
@@ -5,6 +5,6 @@ export class MarkdownEditor{
 
 	constructor(element){
 	    this.element = element;
-    	this.mymarkdowntext = '#WAT ![](http://i0.kym-cdn.com/photos/images/newsfeed/000/173/576/Wat8.jpg)';'
+	    this.mymarkdowntext = '#WAT ![](http://i0.kym-cdn.com/photos/images/newsfeed/000/173/576/Wat8.jpg)';'
 	}
 }
